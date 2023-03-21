ae718d035c55f470b8c9253276d70d9660963de9
Jerry(이재헌) <ti6419@gmail.com>
2023-03-22 08:31:49 +0900

    Initial commit

---
+++ src/main/java/com/violetbeach/redisplayground/RedisplaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.violetbeach.redisplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisplaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisplaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/violetbeach/redisplayground/RedisplaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.violetbeach.redisplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisplaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
